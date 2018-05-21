de06697a26e76138f997127558a78bd6c188cd7c
iiffd <sml.hng@gmail.com>
2018-05-20 21:36:37 -0700

    Refactor reducer

--- src/store/reducers/result.js
+++ src/store/reducers/result.js
@@ -5,13 +5,17 @@ const initialState = {
   results: []
 }
 
+const deleteResult = (state, action) => {
+  const updatedArray = state.results.filter((result) => result.id !== action.deleteId);
+  return updateObject(state, { results: updatedArray });
+}
+
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.STORE_RESULT:
       return updateObject(state, { results: [...state.results, { id: new Date(), value: action.result }] });
     case actionTypes.DELETE_RESULT:
-      const updatedArray = state.results.filter((result) => result.id !== action.deleteId);
-      return updateObject(state, { results: updatedArray });
+      return deleteResult(state, action);
   }
   return state;
 }
